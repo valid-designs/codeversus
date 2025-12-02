6ae6126953cbd9599799e80ddcb85763cbedea78
bipesh <validdesigns562@gmail.com>
2025-12-02 16:33:43 +0545

    hosting

--- apps/frontend/src/axiosInstance.js
+++ apps/frontend/src/axiosInstance.js
@@ -1,19 +1,16 @@
 // src/api/axiosInstance.js
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-// Add JWT token automatically to every request if present
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
   (error) => Promise.reject(error)
